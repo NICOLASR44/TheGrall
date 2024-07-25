496d147dbd7359472f3f842bf699fa7e5e0f0673
lex77darkweb <lex7.7fireblade@gmail.com>
2024-07-26 00:08:03 +0200

    end Accueil

--- thegrall/src/pages/Accueil.jsx
+++ thegrall/src/pages/Accueil.jsx
@@ -1,18 +1,13 @@
 import { Link } from "react-router-dom";
 import "../styles/Accueil.css";
 
-
 export default function Accueil() {
   return (
     <>
       
       <div className="accueilContainer">
-<<<<<<< HEAD:thegrall/src/components/Accueil.jsx
-        <h1 className="accueilTitle">Je suis dans accueil</h1>;
-        <Link to="/dame">DAME</Link>
-=======
-        <h1 className="accueilTitle">Je suis dans composant Accueil</h1>;<Link to="/dame">DAME</Link>
->>>>>>> dev:thegrall/src/pages/Accueil.jsx
+        <h1 className="accueilTitle">The GRAAL</h1>;
+        <Link className="btnLink" to="dame">Enter</Link>
       </div>
     </>
   );
