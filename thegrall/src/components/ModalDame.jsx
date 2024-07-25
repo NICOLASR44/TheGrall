270670218bbaa3b96e4f991e0ea6da605dac0e0c
Sandrine Cazenave-Peyrasson <cazenave.p.sandrine@gmail.com>
2024-07-26 01:04:06 +0200

    added Modal and Scroll components and style files

--- thegrall/src/components/ModalDame.jsx
+++ thegrall/src/components/ModalDame.jsx
@@ -1,5 +1,25 @@
 import "../styles/ModalDame.css";
 
-export default function ModalDame() {
-  return <div>Modal</div>;
+export default function ModalDame({ isOpen, onClose }) {
+  if (!isOpen) return null;
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h1>Bonjour Arthur</h1>
+        <p>
+          Cet objet inconnu que tu tiens dans tes mains, vient du futur.
+          <br />
+          Il va pouvoir t&apos;aider dans ta quête du Graal.
+          <br />
+          Appuie sur le bouton ci-dessous pour commencer ta quête 2.0
+          <br />
+          Ah oui, tu ne sais pas ce que cela veut dire mais fais-moi confiance,
+          tu seras époustoufflé.
+        </p>
+        <button className="modal-button" onClick={onClose}>
+          Fermer
+        </button>
+      </div>
+    </div>
+  );
 }
--- thegrall/src/pages/DameDuLac.jsx
+++ thegrall/src/pages/DameDuLac.jsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
 import "../styles/DameDuLac.css";
+import { useState } from "react";
+import ModalDame from "../components/ModalDame";
+import ScrollDame from "../components/ScrollDame";
 
 export default function DameDuLac() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleClick = () => {
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <>
-      <div className="dameContainer">
-        <h1 className="dameTitle">Bonjour Arthur</h1>
+      <ScrollDame />
+      <div className="dameContainer" onClick={handleClick}>
+        {/* <h1 className="dameTitle">Bonjour Arthur</h1> */}
         <button className="dameButton">
           <Link to="/carte" className="linkToCarte">
-            J&apos;Y VAIS
+            COMMENCER MA QUETE
           </Link>
         </button>
       </div>
+      <ModalDame isOpen={isModalOpen} onClose={handleCloseModal} />
     </>
   );
 }
