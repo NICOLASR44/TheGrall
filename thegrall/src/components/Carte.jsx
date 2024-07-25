17e56037a7d08a8482fd6735a060b88aee505e2e
leodagan <leodaganditlesanguinaire@gmail.com>
2024-07-25 16:47:16 +0200

    bouton avec image

--- thegrall/src/components/Carte.jsx
+++ thegrall/src/components/Carte.jsx
@@ -18,16 +18,19 @@ const Carte = () => {
 					alt="carte du pays de Brocéliande"
 					className="mapBroceliande"
 				/>
-				<div
+
+				<img
+					src="src\assets\saint-graal.png"
+					alt="Graal image"
 					className="map-point"
-					style={{ top: "30%", left: "40%" }}
-					data-info="Point 1"
+					style={{ top: "50%", left: "50%" }}
+					data-info="Grall"
 					onClick={handleClick}
-				></div>
+				></img>
 				<div
 					className="map-point"
 					style={{ top: "50%", left: "60%" }}
-					data-info="Point 2"
+					data-info="Avalon"
 					onClick={handleClick}
 				></div>
 			</div>
@@ -36,44 +39,4 @@ const Carte = () => {
 	);
 };
 
-// 		<div>
-// 			<div className="map-container">
-// 				<img src="votre_carte.jpg" alt="Carte" className="map-image" />
-// 				<div
-// 					className="map-point"
-// 					style={{ top: "30%", left: "40%" }}
-// 					data-info="Point 1"
-// 					onClick={handleClick}
-// 				></div>
-// 				<div
-// 					className="map-point"
-// 					style={{ top: "50%", left: "60%" }}
-// 					data-info="Point 2"
-// 					onClick={handleClick}
-// 				></div>
-// 			</div>
-// 			{info && <div id="info-box">{info}</div>}
-// 		</div>
-// 	);
-// };
-
 export default Carte;
-
-/* Code Sandrine
-export default function Carte() {
-	return (
-		<>
-			<div className="carteContainer">
-				<h1 className="carteTitle">Je suis dans composant Carte</h1>;
-				<div className="mapBroceliande">
-					<img
-						src="src\assets\map Brocéliande.jpg"
-						alt="carte du pays de Brocéliande"
-						className="mapBroceliande"
-					/>
-				</div>
-			</div>
-		</>
-	);
-}
-*/
