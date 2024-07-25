8ebbbd008cff7b5c61a387513fded74a7b380d55
lex77darkweb <lex7.7fireblade@gmail.com>
2024-07-25 17:30:46 +0200

    modif accueil

--- thegrall/src/components/Accueil.jsx
+++ thegrall/src/components/Accueil.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import "../styles/Accueil.css";
 
+
 export default function Accueil() {
   return (
     <>
+      
       <div className="accueilContainer">
-        <h1 className="accueilTitle">Je suis dans composant Accueil</h1>;
+        <h1 className="accueilTitle">Je suis dans accueil</h1>;
         <Link to="/dame">DAME</Link>
       </div>
     </>
